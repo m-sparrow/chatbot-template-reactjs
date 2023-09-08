b9bf303e57e0d0b5b5c2a34cb82f7fb1e6879dfd
m-sparrow <hari.tinyblitz@gmail.com>
2023-09-09 00:55:05 +0530

    updated chat message window width

--- src/comp/Message.js
+++ src/comp/Message.js
@@ -7,13 +7,13 @@ export default function Message(props) {
     const d = new Date();
 
     const [date] = useState(
-        d.toLocaleString().slice(0,20)
+        d.toLocaleString()
     );
 
     return(
         <React.Fragment>
             <Box sx={{ display: 'flex', width: '75vw', direction: 'row', justifyContent: props.msgType === 'user' ? 'flex-end' :  'flex-start'}}>
-                <Card sx={{ m: 1, maxWidth: '30vw', backgroundColor: '#edf2f5' }}>
+                <Card sx={{ m: 1, maxWidth: '60vw', backgroundColor: '#edf2f5' }}>
                     <CardHeader
                         avatar = {
                             <Avatar sx={{ bgcolor: props.msgType === 'user' ? blue[400] : red[400] }} aria-label={props.userName}>
